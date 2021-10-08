0a085a86413802007a6eecb6eeaf92a09df57f1e
ankushc13 <devilchauhan0@gmail.com>
2021-10-08 19:53:01 +0530

    Initial commit

---
+++ src/app/coach/coachhome/coachhome.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { CoachhomeService } from './coachhome.service';
+
+@Component({
+  selector: 'app-coachhome',
+  templateUrl: './coachhome.component.html',
+  styleUrls: ['./coachhome.component.css']
+})
+export class CoachhomeComponent implements OnInit {
+  id!:number;
+  schedulesDetail!:any[]
+  constructor(private coachhomeService:CoachhomeService){}
+  ngOnInit() {
+    this.id=parseInt(sessionStorage.getItem("loggedUser") || '0');
+    this.schedules();
+  }
+  schedules(){
+    this.schedulesDetail=[]
+    this.coachhomeService.schedules().subscribe(res=>{
+      for(const obj of res){
+        if(this.id===getId(obj)){
+          this.schedulesDetail.push(obj);
+        }
+      }
+      console.log(this.schedulesDetail);
+    }
+    );
+  }
+
+}
+
+function getId(res:any):any{
+  let id;
+  for (const [key, value] of Object.entries(res)) {
+    if(key=='coachId'){
+      id=value;
+    }
+  }
+  return id;
+}
